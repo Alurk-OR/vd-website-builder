b24e0806675a6ec8a3de34cf604a401a72eb5d93
Alurk-OR <abasevanton447@yandex.ru>
2026-02-26 23:09:42 +0300

    What is magic?

--- VDWebsiteBuilder/Models/VisualElement.cs
+++ VDWebsiteBuilder/Models/VisualElement.cs
@@ -6,8 +6,7 @@
         public string Tag { get; set; } = "p";
         public string Content { get; set; } = "";
         public string FontSize { get; set; } = "18";
-        public string TextAlign { get; set; } = "left";
-        public string FontFamily { get; set; } = "Arial, sans-serif";
+
         public string Color { get; set; } = "#000000";
         public string GetHtmlCode() => $"<{Tag} style=\"color: {Color};\">{Content}</{Tag}>";
         public string GetExplanation() => Tag switch {
