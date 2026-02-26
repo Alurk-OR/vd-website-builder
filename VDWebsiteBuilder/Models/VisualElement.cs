be8642490423c445aa0ec00aea562001fb8de77e
Alurk-OR <abasevanton447@yandex.ru>
2026-02-26 22:00:03 +0300

    Added font characteristics

--- VDWebsiteBuilder/Models/VisualElement.cs
+++ VDWebsiteBuilder/Models/VisualElement.cs
@@ -2,8 +2,12 @@
 {
     public class VisualElement
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Tag { get; set; } = "p";
         public string Content { get; set; } = "";
+        public string FontSize { get; set; } = "18";
+        public string TextAlign { get; set; } = "left";
+        public string FontFamily { get; set; } = "Arial, sans-serif";
         public string Color { get; set; } = "#000000";
         public string GetHtmlCode() => $"<{Tag} style=\"color: {Color};\">{Content}</{Tag}>";
         public string GetExplanation() => Tag switch {
